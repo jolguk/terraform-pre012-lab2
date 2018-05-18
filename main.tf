67e4661308df6300cbeb9580ddf48906a179488d
Richard Cheney <richard.cheney@microsoft.com>
2018-05-18 14:01:55 +0100

    Matches lab description

--- main.tf
+++ main.tf
@@ -4,11 +4,16 @@ resource "azurerm_resource_group" "citadel" {
     tags                      = "${var.tags}" 
 }
 
+resource "random_id" "rnd" {
+    keepers = {
+        rg = "${azurerm_resource_group.citadel.id}"
+    }
+    byte_length = 6
+}
 resource "azurerm_storage_account" "sa" {
-    name                      = "${var.tags["source"]}richeneysa1976"
+    name                      = "sa${var.tags["source"]}${random_id.rnd.hex}"
     resource_group_name       = "${azurerm_resource_group.citadel.name}"
     location                  = "${azurerm_resource_group.citadel.location}"
     tags                      = "${azurerm_resource_group.citadel.tags}" 
-    account_tier              = "Standard"
     account_replication_type  = "LRS"
 }
