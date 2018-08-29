63af519d8c3680e1464608ab0b6f9dc59c303dc4
Richard Cheney <richard.cheney@microsoft.com>
2018-08-29 15:34:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
-resource "azurerm_resource_group" "citadel" {
-    name                      = "${var.rg}"
-    location                  = "${var.loc}"
-    tags                      = "${var.tags}" 
+resource "azurerm_resource_group" "lab2" {
+  name     = "${var.rg}"
+  location = "${var.loc}"
+  tags     = "${var.tags}"
 }
 
-resource "random_id" "rnd" {
-    keepers = {
-        rg = "${azurerm_resource_group.citadel.id}"
-    }
-    byte_length = 6
+resource "random_string" "rnd" {
+  length  = 8
+  lower   = true
+  number  = true
+  upper   = false
+  special = false
 }
-resource "azurerm_storage_account" "sa" {
-    name                      = "sa${var.tags["source"]}${random_id.rnd.hex}"
-    resource_group_name       = "${azurerm_resource_group.citadel.name}"
-    location                  = "${azurerm_resource_group.citadel.location}"
-    tags                      = "${azurerm_resource_group.citadel.tags}" 
-    account_replication_type  = "LRS"
+
+resource "azurerm_storage_account" "lab2sa" {
+  name                     = "sa${var.tags["source"]}${random_string.rnd.result}"
+  resource_group_name      = "${azurerm_resource_group.lab2.name}"
+  location                 = "${azurerm_resource_group.lab2.location}"
+  tags                     = "${azurerm_resource_group.lab2.tags}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
